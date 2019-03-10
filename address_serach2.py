10be053b3b17c26a62fe00a05e706bfc4a7cf227
Sato <s_ychm@yahoo.co.jp>
2019-03-10 17:58:44 +0900

    mainを追加

--- address_serach2.py
+++ address_serach2.py
@@ -1,15 +1,16 @@
 import requests
 
-if __name__ == '__main__':
-    zipcode = '7900046'
+
+def main():
+    zipcode = input('郵便番号(7桁)?: ')
     url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
-
     r = requests.get(url)
-
     address_dict = r.json()
-
     address1 = address_dict['results'][0]['address1']
     address2 = address_dict['results'][0]['address2']
     address3 = address_dict['results'][0]['address3']
-
     print(f'{address1}{address2}{address3}')
+
+
+if __name__ == '__main__':
+    main()
