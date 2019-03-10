8b71c9554cf93bb459b23138046905dc4c1108eb
Sato <s_ychm@yahoo.co.jp>
2019-03-10 16:27:21 +0900

    辞書の練習

---
+++ dictpractice.py
@@ -0,0 +1,26 @@
+address_data = [{"address1": "岩手県",
+                 "address2": "八幡平市",
+                 "address3": "大更",
+                 "zipcode": "0287111"},
+
+                {"address1": "兵庫県",
+                 "address2": "西宮市",
+                 "address3": "高須町",
+                 "zipcode": "6638141"},
+                ]
+
+if __name__ == '__main__':
+    # 兵庫県西宮市高須町 と出力してください
+    address1 = address_data[1]['address1']
+    address2 = address_data[1]['address2']
+    address3 = address_data[1]['address3']
+
+    print(f'{address1}{address2}{address3}')
+
+    # 岩手県八幡平市大更 と出力してください
+
+    address1 = address_data[0]['address1']
+    address2 = address_data[0]['address2']
+    address3 = address_data[0]['address3']
+
+    print(f'{address1}{address2}{address3}')
