a6b49f7e3e6470a1d28e1fa36952f1e4a60e1b56
Sato <s_ychm@yahoo.co.jp>
2019-03-11 23:33:13 +0900

    不正番号の際に｢該当するデータは見つかりませんでした｣を表示

--- address_serach2.py
+++ address_serach2.py
@@ -6,10 +6,17 @@ def main():
     url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
     r = requests.get(url)
     address_dict = r.json()
-    address1 = address_dict['results'][0]['address1']
-    address2 = address_dict['results'][0]['address2']
-    address3 = address_dict['results'][0]['address3']
-    print(f'{address1}{address2}{address3}')
+
+    result = address_dict['results']
+
+    if result is not None:
+        address1 = address_dict['results'][0]['address1']
+        address2 = address_dict['results'][0]['address2']
+        address3 = address_dict['results'][0]['address3']
+
+        print(f'{address1}{address2}{address3}')
+    else:
+        print('該当するデータは見つかりませんでした')
 
 
 if __name__ == '__main__':
